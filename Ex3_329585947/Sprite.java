18a58a7a9710789f558f1f54395b82760a2bca2e
TehilaGurwicz <tamar23cohen@gmail.com>
2026-03-31 12:18:04 +0300

    Delete Ex3_329585947 directory

--- Ex3_329585947/Sprite.java
+++
@@ -1,8 +0,0 @@
-import biuoop.DrawSurface;
-
-public interface Sprite {
-    // draw the sprite to the screen
-    void drawOn(DrawSurface d);
-    // notify the sprite that time has passed
-    void timePassed();
-}
